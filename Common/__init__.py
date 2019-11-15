420050c4cb4c3299d946763add2b41008317a5f8
wangby-101 <15942123960@163.com>
2019-11-15 13:54:57 +0800

    文件目录

---
+++ Common/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/15 13:33
+# @Author  : Wang Bingyin
+# @Email   : xxxxxxx@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Common/base_path.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/15 13:40
+# @Author  : Wang Bingyin
+# @Email   : xxxxxxx@163.com
+# @File    : base_path.py
+# @Software: PyCharm
+
+import os
+
+basepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 测试用例
+data_dir = os.path.join(basepath, "Test_Data")
+data_file = os.path.join(data_dir, "cases.xlsx")
+if __name__ == '__main__':
+    print(data_file)
