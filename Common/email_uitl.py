3b6be0cf6a4417f221e8758c3e1abed31a130cfe
wangby-101 <15942123960@163.com>
2019-11-22 14:34:46 +0800

    邮件修改

--- Common/email_uitl.py
+++ Common/email_uitl.py
@@ -6,12 +6,13 @@
 # @Software: PyCharm
 import smtplib
 from Common.base_path import reports_html
+from Common.log import MyLogger
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 class SendMail(object):
-    def __init__(self,username,passwd,recv,title,content,
-                 file=None,
-                 email_host='smtp.163.com',port=25):
+
+    def __init__(self, username, passwd, recv, title, content, file=None,
+                 email_host='smtp.163.com', port=25):
         self.username = username
         self.passwd = passwd
         self.recv = recv
@@ -31,22 +32,23 @@ class SendMail(object):
         msg.attach(MIMEText(self.content))#邮件正文的内容
         msg['Subject'] = self.title  # 邮件主题
         msg['From'] = self.username  # 发送者账号
-        msg['To'] = self.recv  # 接收者账号列表
-        self.smtp = smtplib.SMTP(self.email_host,port=self.port)
+        msg['To'] = ",".join(self.recv)  # 接收者账号列表
+        self.smtp = smtplib.SMTP(self.email_host, port=self.port)
         #发送邮件服务器的对象
-        self.smtp.login(self.username,self.passwd)
+        self.smtp.login(self.username, self.passwd)
         try:
-            self.smtp.sendmail(self.username,self.recv,msg.as_string())
+            self.smtp.sendmail(self.username, self.recv, msg.as_string())
         except Exception as e:
-            print('出错了。。',e)
+            MyLogger().info('出错了。。', e)
+            raise e
         else:
-            print('发送成功！')
+            MyLogger().info('发送成功！')
     def __del__(self):
         self.smtp.quit()
 
 if __name__ == '__main__':
     m = SendMail(
-        username='15942123962@163.com', passwd='qwe12345', recv='wangbingyin-zkhtg@gome.com.cn',
+        username='15942123962@163.com', passwd='qwe12345', recv=['zkht_zhangyue@gome.com.cn', 'wangbingyin-zkhtg@gome.com.cn'],
         title='新鞋的发送邮件', content='哈哈哈啊哈哈哈哈', file=reports_html
     )
     m.send_mail()
--- run.py
+++ run.py
@@ -8,6 +8,7 @@
 import unittest
 import HTMLTestRunnerNew
 from Common import base_path
+from Common.email_uitl import SendMail
 
 # 自动查找testcases目录下，以test开头的.py文件里面的测试类
 discover = unittest.defaultTestLoader.discover(base_path.testcases_dir, pattern="test*.py", top_level_dir=None)
@@ -19,3 +20,9 @@ with open(base_path.reports_html, 'wb') as file:
                                               description='API测试报告',
                                               tester='王柄印')
     runner.run(discover)  # 执行查找到的用例
+
+m = SendMail(
+        username='15942123962@163.com', passwd='qwe12345', recv=['zkht_zhangyue@gome.com.cn', 'wangbingyin-zkhtg@gome.com.cn'],
+        title='API测试报告', content='邮件内容', file=base_path.reports_html
+    )
+m.send_mail()
